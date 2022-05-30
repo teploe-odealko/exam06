e11bc6bc68f5ddcd9127826a4774eb80beda24d8
din Lat <dinlat@MacBook-Air-din.local>
2022-05-30 19:59:24 +0300

    final

--- mini_serv.c
+++ mini_serv.c
@@ -5,35 +5,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "EndlessLoop"
+
+const int BUF_SIZE = 42 * 4096;
 typedef struct client {
     int id;
     char msg[110000];
 } t_client;
 
-t_client clients[100];
+t_client clients[1024];
 
 int max = 0, next_id = 0;
 fd_set active, readyRead, readyWrite;
-char bufRead[42*4096], str[42*4096], bufWrite[42*4097];
-void fatal_error()
-{
+char bufRead[BUF_SIZE], bufWrite[BUF_SIZE];
+
+void fatal_error() {
     write(2, "Fatal error\n", 12);
     exit(1);
 }
+
 void send_all(int es) {
     for (int i = 0; i <= max; i++)
         if (FD_ISSET(i, &readyWrite) && i != es)
             send(i, bufWrite, strlen(bufWrite), 0);
 }
+
 int main(int ac, char **av) {
     if (ac != 2) {
         write(2, "Wrong number of arguments\n", 26);
         exit(1);
     }
     int port = atoi(av[1]);
-    (void) port;
 
     bzero(&clients, sizeof(clients));
     FD_ZERO(&active);
@@ -67,26 +68,22 @@ int main(int ac, char **av) {
                 int clientSock = accept(serverSock, (struct sockaddr *)&addr, &addr_len);
                 if (clientSock < 0)
                     continue ;
-
                 max = (clientSock > max) ? clientSock : max;
                 clients[clientSock].id = next_id++;
                 FD_SET(clientSock, &active);
-
                 sprintf(bufWrite, "server: client %d just arrived\n", clients[clientSock].id);
                 send_all(clientSock);
-//                break ;
+                break ;
             }
 
             if (FD_ISSET(s, &readyRead) && s != serverSock) {
-
                 int res = recv(s, bufRead, 42*4096, 0);
-
                 if (res <= 0) {
                     sprintf(bufWrite, "server: client %d just left\n", clients[s].id);
                     send_all(s);
                     FD_CLR(s, &active);
                     close(s);
-//                    break ;
+                    break ;
                 }
                 else {
                     for (int i = 0, j = strlen(clients[s].msg); i < res; i++, j++) {
@@ -99,9 +96,9 @@ int main(int ac, char **av) {
                             j = -1;
                         }
                     }
+                    break;
                 }
             }
         }
     }
 }
-#pragma clang diagnostic pop
